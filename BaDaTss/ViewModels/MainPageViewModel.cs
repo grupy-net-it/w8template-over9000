47a9224800fbd11ebe78e61228d9020c2070a689
Kamil Cie�lak <keraxel@gmail.com>
2013-06-02 16:01:39 +0200

    Init

---
+++ BaDaTss/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using StyleMVVM.DependencyInjection;
+using StyleMVVM.ViewModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BaDaTss.ViewModels
+{
+	public class MainPageViewModel : PageViewModel
+	{
+		
+	}
+}
---
+++ BaDaTss/Views/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using StyleMVVM.View;
+
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using LayoutAwarePage = BaDaTss.Common.LayoutAwarePage;
+
+namespace BaDaTss
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    [StartPage]
+    public sealed partial class MainPage : LayoutAwarePage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var snd = new MediaElement();
+            StorageFolder folder = await Package.Current.InstalledLocation.GetFolderAsync("Sounds");
+            StorageFile file = await folder.GetFileAsync("sound.mp3");
+            var stream = await file.OpenAsync(FileAccessMode.Read);
+            snd.SetSource(stream, file.ContentType);
+            snd.Play();
+        }
+    }
+}
